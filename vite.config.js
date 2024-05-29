29eca458282e7fa20518272efd5ec5ed7850a0bc
markrestart <markrestart@gmail.com>
2024-05-29 09:45:55 -0400

    more sentry config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/salad/",
-  plugins: [react()],
+
+  plugins: [react(), sentryVitePlugin({
+    org: "funqualified",
+    project: "funqualified-apps"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 });
