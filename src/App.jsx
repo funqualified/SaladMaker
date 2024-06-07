c9c64882445cd184898a047b3244cea67640a2cf
markrestart <markrestart@gmail.com>
2024-06-07 12:23:56 -0400

    Information modal done!

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,26 @@ import Tab from "@mui/joy/Tab";
 import TabPanel from "@mui/joy/TabPanel";
 import Box from "@mui/joy/Box";
 
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import ModalClose from "@mui/joy/ModalClose";
+import Typography from "@mui/joy/Typography";
+import InfoIcon from "@mui/icons-material/Info";
+
 import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
 import GlobalStyles from "@mui/joy/GlobalStyles";
 import CssBaseline from "@mui/joy/CssBaseline";
 
 import { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
+import { Button, Link } from "@mui/joy";
 
 function App() {
   const [saladRules, setSaladRulesInternal] = useState({ excludedIngredients: [], saladProfile: [] });
   const [cookie, setCookie] = useCookies(["token", "id", "rules"]);
   const [username, setUsername] = useState("");
   const [initialized, setInitialized] = useState(false);
+  const [infoOpen, setinfoOpen] = useState(false);
   const APIurl = import.meta.env.VITE_APP_API_URL;
 
   const setSaladRules = (rules) => {
@@ -61,6 +69,8 @@ function App() {
     };
     if (cookie.saladMaker_rules) {
       rules = { ...rules, ...cookie.saladMaker_rules };
+    } else {
+      setinfoOpen(true);
     }
 
     const token = cookie.token;
@@ -99,6 +109,7 @@ function App() {
 
   return (
     <CssVarsProvider defaultMode="dark" disableTransitionOnChange>
+      <InfoIcon onClick={() => setinfoOpen(true)} style={{ position: "fixed", top: "10px", left: "10px", borderRadius: "50%" }} />
       <CssBaseline />
       <GlobalStyles
         styles={{
@@ -111,6 +122,22 @@ function App() {
         }}
       />
       <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column" }}>
+        <Modal open={infoOpen} onClose={() => setinfoOpen(false)}>
+          <ModalDialog>
+            <ModalClose />
+            <Typography level="h1">Welcome to the Salad Maker</Typography>
+            <Typography>
+              Salad Maker is a project by Ryan Lindemulder for Funqualified. It began with TikToks of him making unusual salads. Now this web app is here for
+              him and others to get crazy salad recipes
+            </Typography>
+            <Typography>Click the tabs to make a salad or personalize your salad profile.</Typography>
+            <Typography>
+              <Link href="https://app.funqualified.com/dashboard/login?redirect=https://app.funqualified.com/salad">Signing in</Link> to an account will let you
+              use your salad profile from any device.
+              {/*TODO: add this text: You can also save your favorite salads.  */}
+            </Typography>
+          </ModalDialog>
+        </Modal>
         <h1>Salad Maker</h1>
         <Tabs defaultValue={0}>
           <TabList>
